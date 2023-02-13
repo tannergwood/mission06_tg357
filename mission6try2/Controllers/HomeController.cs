b94ff04b8f2eb63df4ade0071e07da885cfc7031
tannergwood <tg357@byu.edu>
2023-02-13 09:02:49 -0700

    Add project files.

---
+++ mission6try2/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mission6try2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission6try2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext _movieContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, MovieContext movie)
+        {
+            _logger = logger;
+            _movieContext = movie;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieInput()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieInput(MovieInput mv)
+        {
+            _movieContext.Add(mv);
+            _movieContext.SaveChanges();
+            return View("MovieSubmitted", mv);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ mission6try2/Models/MovieContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission6try2.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MovieInput> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieInput>().HasData(
+                
+                new MovieInput
+                {
+                    MovieID = 1,
+                    Title = "The Sandlot",
+                    Category = "Family",
+                    Year = 1993,
+                    Director = "David Mickey Evans",
+                    Rating = "PG", 
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Great Movie"
+                },
+                new MovieInput
+                {
+                    MovieID = 2,
+                    Title = "Cool Runnings",
+                    Category = "Comedy",
+                    Year = 1993,
+                    Director = "Jon Turteltaub",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Good Movie"
+                },
+                new MovieInput
+                {
+                    MovieID = 3,
+                    Title = "42",
+                    Category = "Drama",
+                    Year = 2013,
+                    Director = "Brian Helgeland",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Super Movie"
+                }
+
+                );
+        }
+    }
+}
